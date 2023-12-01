1a20ae957be88450967bacde8a8d86116d9ad45f
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-12-01 21:01:15 +0100

    optimized the thing

--- src/main/kotlin/de/maiker/adventofcode2023/day1/Solution.kt
+++ src/main/kotlin/de/maiker/adventofcode2023/day1/Solution.kt
@@ -14,44 +14,55 @@ fun main() {
 
 fun partOne(lines: List<String>): String {
     val numbers = lines.map { line ->
-        line
-            .filter { it.isDigit() }
-            .let { listOf(it.first(), it.last()) }
-            .joinToString("")
-            .toInt()
+        val first = line.first { it.isDigit() }.digitToInt()
+        val last = line.last { it.isDigit() }.digitToInt()
+
+        first * 10 + last
     }
 
     return numbers.sum().toString()
 }
 
-val stringDigits = listOf(
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine"
+val digits = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
 )
 
-fun partTwo(lines: List<String>): String {
-    val numbers = lines.map { line ->
-        val digits = emptyList<String>().toMutableList()
-        line.forEachIndexed { index, character ->
-            if (character.isDigit()) {
-                digits.add(character.toString())
-                return@forEachIndexed
-            }
+fun findAtIndex(line: String, index: Int): Int? {
+    if (line[index].isDigit()) {
+        return line[index].digitToInt()
+    }
 
-            stringDigits.forEachIndexed { digit, stringDigit ->
-                if (line.take(index + 1).takeLast(stringDigit.length) == stringDigit) {
-                    digits.add((digit + 1).toString())
-                }
-            }
+    digits.forEach { (digitAsWord, digit) ->
+        if (line.take(index + 1).takeLast(digitAsWord.length) == digitAsWord) {
+            return digit
         }
-        listOf(digits.first(), digits.last()).joinToString("").toInt()
+    }
+
+    return null
+}
+
+fun find(line: String, index: Int, dir: Int): Int {
+    return findAtIndex(line, index) ?: find(line, index + dir, dir)
+}
+
+fun leftFind(line: String) = find(line, 0, 1)
+
+fun rightFind(line: String) = find(line, line.length - 1, -1)
+
+fun partTwo(lines: List<String>): String {
+    val numbers = lines.map {
+        val first = leftFind(it)
+        val last = rightFind(it)
+
+        first * 10 + last
     }
 
     return numbers.sum().toString()
