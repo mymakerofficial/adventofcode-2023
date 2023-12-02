cf39cd30ba73060674bf8d845156251f4db47bc2
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-12-02 15:16:55 +0100

    add day 2 part one

---
+++ src/main/kotlin/de/maiker/adventofcode2023/day2/Sokution.kt
@@ -0,0 +1,68 @@
+package de.maiker.adventofcode2023.day2
+
+import java.io.File
+
+fun main() {
+    val lines = File("src/main/resources/day2.txt").readLines()
+
+    val partOne = partOne(lines)
+    println("part one: $partOne")
+
+    val partTwo = partTwo(lines)
+    println("part two: $partTwo")
+}
+
+val legalAmounts = mapOf(
+    "red" to 12,
+    "green" to 13,
+    "blue" to 14,
+)
+
+fun String.getFirstThat(predicate: (Char) -> Boolean): String {
+    return this.dropWhile { !predicate(it) }.takeWhile(predicate)
+}
+
+fun String.getFirstNumber(): Int {
+    return this.getFirstThat { it.isDigit() }.toInt()
+}
+
+fun parseLine(line: String): Pair<Int, List<Pair<String, Int>>> {
+    val id = line.getFirstNumber()
+
+    val events = line
+        .replace(",", "")
+        .replace(";", "")
+        .dropWhile { it != ':' }
+        .drop(2)
+        .split(" ")
+        .windowed(2, 2)
+        .map { (amount, color) ->
+            color to amount.toInt()
+        }
+
+    return id to events
+}
+
+fun partOne(lines: List<String>): String {
+    val sum = lines
+        .map {parseLine(it) }
+        .map { (id, events) ->
+            val invalid = events.any { (color, amount) ->
+                legalAmounts[color]!! < amount
+            }
+
+            id to !invalid
+        }.sumOf { (id, valid) ->
+            if (valid) {
+                id
+            } else {
+                0
+            }
+        }
+
+    return sum.toString()
+}
+
+fun partTwo(lines: List<String>): String {
+    TODO()
+}
