5e7a0d42f2e2da5ce7815df89ab7b9a199689085
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-12-04 20:27:22 +0100

    refactor part one

--- src/main/kotlin/de/maiker/adventofcode2023/day3/Solution.kt
+++ src/main/kotlin/de/maiker/adventofcode2023/day3/Solution.kt
@@ -16,107 +16,7 @@ fun Char.isSymbol(): Boolean {
     return !this.isDigit() && !this.isLetter() && this != '.'
 }
 
-fun hasAdjacentSymbol(lines: Triple<String?, String, String?>, startIndex: Int, endIndex: Int): Boolean {
-    val (previous, current, next) = lines
-
-    val marginStartIndex = if (startIndex == 0) 0 else startIndex - 1
-    val marginEndIndex = if (endIndex == current.length - 1) endIndex else endIndex + 1
-
-    val previousContains = previous?.slice(marginStartIndex..marginEndIndex)?.any { it.isSymbol() } ?: false
-    val currentContains = current.slice(marginStartIndex..marginEndIndex).any { it.isSymbol() }
-    val nextContains = next?.slice(marginStartIndex..marginEndIndex)?.any { it.isSymbol() } ?: false
-
-    return previousContains || currentContains || nextContains
-}
-
-fun findNumbersWithIndices(line: String): List<Triple<Int /*number*/, Int /*start*/, Int /*end*/>> {
-    val digitsWithIndices = line
-        .mapIndexed { index, c ->
-            index to c
-        }
-        .filter { (_, c) ->
-            c.isDigit()
-        }
-
-    val groupedDigitsWithIndices = digitsWithIndices.fold(mutableListOf<MutableList<Pair<Int, Char>>>()) { acc, element ->
-        val (index, char) = element
-        val last = acc.lastOrNull()
-
-        if (last?.lastOrNull()?.first == index - 1) {
-            last.add(index to char)
-        } else {
-            acc.add(mutableListOf(index to char))
-        }
-
-        acc
-    }
-
-    return groupedDigitsWithIndices.map {
-        val start = it.first().first
-        val end = it.last().first
-
-        val number = it.map { (_, char) -> char }.joinToString("").toInt()
-
-        Triple(
-            number,
-            start,
-            end
-        )
-    }
-}
-
-fun partOne(lines: List<String>): String {
-    val sums = lines.mapIndexed { index, line ->
-        val currentLines = Triple(
-            lines.getOrNull(index - 1),
-            line,
-            lines.getOrNull(index + 1)
-        )
-
-        val numbersWithIndices = findNumbersWithIndices(line)
-
-        val numbersWithAdjacentSymbol = numbersWithIndices.filter { (_, start, end) ->
-            hasAdjacentSymbol(currentLines, start, end)
-        }
-
-        val sum = numbersWithAdjacentSymbol.sumOf { (number, _, _) ->
-            number
-        }
-
-        sum
-    }
-
-    return sums.sum().toString()
-}
-
-class Symbol(
-    val x: Int,
-    val y: Int,
-    val char: Char,
-) {
-    override fun toString() = "Symbol(x=$x, y=$y, char=$char)"
-
-    fun isGear() = char == '*'
-
-    infix fun isAdjacentTo(partNumber: PartNumber) = partNumber isAdjacentTo this
-}
-
-class PartNumber(
-    val x: Int,
-    val y: Int,
-    val length: Int,
-    val number: Int,
-) {
-    override fun toString() = "Number(x=$x, y=$y, length=$length, number=$number)"
-
-    infix fun isAdjacentTo(symbol: Symbol): Boolean {
-        val horizontalRange = (x - 1)..(x + length)
-        val verticalRange = (y - 1)..(y + 1)
-        return symbol.x in horizontalRange && symbol.y in verticalRange
-    }
-}
-
-fun partTwo(lines: List<String>): Int {
+fun parseLines(lines: List<String>): Pair<List<Symbol>, List<PartNumber>> {
     val symbols = mutableListOf<Symbol>()
     val partNumbers = mutableListOf<PartNumber>()
 
@@ -145,6 +45,54 @@ fun partTwo(lines: List<String>): Int {
         }
     }
 
+    return symbols to partNumbers
+}
+
+class Symbol(
+    val x: Int,
+    val y: Int,
+    val char: Char,
+) {
+    override fun toString() = "Symbol(x=$x, y=$y, char=$char)"
+
+    fun isGear() = char == '*'
+}
+
+class PartNumber(
+    val x: Int,
+    val y: Int,
+    val length: Int,
+    val number: Int,
+) {
+    override fun toString() = "Number(x=$x, y=$y, length=$length, number=$number)"
+
+    infix fun isAdjacentTo(symbol: Symbol): Boolean {
+        val horizontalRange = (x - 1)..(x + length)
+        val verticalRange = (y - 1)..(y + 1)
+        return symbol.x in horizontalRange && symbol.y in verticalRange
+    }
+
+    infix fun isAdjacentToAny(symbols: List<Symbol>) =
+        symbols.any { symbol ->
+            this isAdjacentTo symbol
+        }
+}
+
+fun partOne(lines: List<String>): Int {
+    val (symbols, partNumbers) = parseLines(lines)
+
+    val numbers = partNumbers.filter { partNumber ->
+        partNumber isAdjacentToAny symbols
+    }.map {
+        it.number
+    }
+
+    return numbers.sum()
+}
+
+fun partTwo(lines: List<String>): Int {
+    val (symbols, partNumbers) = parseLines(lines)
+
     val gearRatios = symbols
         .filter { it.isGear() }
         .map { gear ->
@@ -163,8 +111,6 @@ fun partTwo(lines: List<String>): Int {
 
             gearRatio
         }
-
-    gearRatios.forEach { println(it) }
 
     return gearRatios.sum()
 }
