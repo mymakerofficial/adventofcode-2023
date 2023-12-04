4c54d18719e5f3b2653275d5245f02c5fcd275c6
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-12-04 20:09:08 +0100

    add day 3 part two

--- src/main/kotlin/de/maiker/adventofcode2023/day3/Solution.kt
+++ src/main/kotlin/de/maiker/adventofcode2023/day3/Solution.kt
@@ -89,6 +89,82 @@ fun partOne(lines: List<String>): String {
     return sums.sum().toString()
 }
 
-fun partTwo(lines: List<String>): String {
-    return ""
+class Symbol(
+    val x: Int,
+    val y: Int,
+    val char: Char,
+) {
+    override fun toString() = "Symbol(x=$x, y=$y, char=$char)"
+
+    fun isGear() = char == '*'
+
+    infix fun isAdjacentTo(partNumber: PartNumber) = partNumber isAdjacentTo this
+}
+
+class PartNumber(
+    val x: Int,
+    val y: Int,
+    val length: Int,
+    val number: Int,
+) {
+    override fun toString() = "Number(x=$x, y=$y, length=$length, number=$number)"
+
+    infix fun isAdjacentTo(symbol: Symbol): Boolean {
+        val horizontalRange = (x - 1)..(x + length)
+        val verticalRange = (y - 1)..(y + 1)
+        return symbol.x in horizontalRange && symbol.y in verticalRange
+    }
+}
+
+fun partTwo(lines: List<String>): Int {
+    val symbols = mutableListOf<Symbol>()
+    val partNumbers = mutableListOf<PartNumber>()
+
+    lines.forEachIndexed { y, line ->
+        val currentDigits = mutableListOf<Char>()
+        "$line.".forEachIndexed { x, it ->
+            if (it.isDigit()) {
+                currentDigits.add(it)
+            } else if (currentDigits.isNotEmpty()) {
+                partNumbers.add(PartNumber(
+                    x = x - currentDigits.size,
+                    y = y,
+                    length = currentDigits.size,
+                    number = currentDigits.joinToString("").toInt()
+                ))
+                currentDigits.clear()
+            }
+
+            if (it.isSymbol()) {
+                symbols.add(Symbol(
+                    x = x,
+                    y = y,
+                    char = it,
+                ))
+            }
+        }
+    }
+
+    val gearRatios = symbols
+        .filter { it.isGear() }
+        .map { gear ->
+            val adjacentPartNumbers = partNumbers.filter { partNumber ->
+                partNumber isAdjacentTo gear
+            }
+
+            if (adjacentPartNumbers.size != 2) {
+                // a symbol is only actually a gear if it is adjacent to exactly two part numbers
+                return@map 0
+            }
+
+            val (first, second) = adjacentPartNumbers
+
+            val gearRatio = first.number * second.number
+
+            gearRatio
+        }
+
+    gearRatios.forEach { println(it) }
+
+    return gearRatios.sum()
 }
